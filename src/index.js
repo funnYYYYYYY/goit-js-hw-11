58aa50638a7294dfbc93b6627a7cce52e95abe07
Pavlo <pasam9300@gmail.com>
2022-10-02 14:08:02 +0200

    Update

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import axios from 'axios';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
-
 axios.defaults.baseURL = 'https://pixabay.com/api/';
-
 const refs = {
   form: document.querySelector('#search-form'),
   gallery: document.querySelector('.gallery'),
@@ -12,51 +10,41 @@ const refs = {
 let value = '';
 let currentPage = 1;
 const HITS_PER_PAGE = 40;
+let totalPages = 0;
 
 let items = [];
-
 refs.form.addEventListener('submit', onSearchItems);
-
 function onSearchItems(e) {
   e.preventDefault();
   value = e.currentTarget.elements.searchQuery.value;
   refs.loadMore.classList.remove('visible');
-
   if (value.trim() === '') {
     Notify.failure(
       'Sorry, there are no images matching your search query. Please try again.'
     );
     return;
   }
-
   currentPage = 1;
   refs.gallery.innerHTML = '';
   getPicture(value.trim());
 }
-
 function onClickLoadMore() {
   currentPage += 1;
-
   getPicture();
 }
-
 const getPicture = async () => {
   try {
     const { data } = await axios.get(
       `?q=${value}&image_type=photo&orientation=horizontal&safesearch=true&per_page=${HITS_PER_PAGE}&page=${currentPage}&key=30191539-d56ffab2c88cb867d9bceaf74`
     );
-
-    items = [...items, data.hits];
-
+    items = [...items, ...data.hits];
     renderList(data.hits);
-
     if (data.totalHits === 0) {
       Notify.failure(
         'Sorry, there are no images matching your search query. Please try again.'
       );
       return;
     }
-
     if (data.totalHits === data.totalHits.length - 1) {
       Notify.failure(
         "We're sorry, but you've reached the end of search results."
@@ -64,11 +52,16 @@ const getPicture = async () => {
       return;
     }
     refs.loadMore.classList.add('visible');
+
+    totalPages = Math.ceil(totalHits / 40);
+
+    if (currentPage >= totalPages) {
+      refs.loadMore.classList.remove('visible');
+    }
   } catch (error) {
     Notify.failure(error);
   }
 };
-
 const renderList = items => {
   const list = items
     .map(
@@ -101,5 +94,4 @@ const renderList = items => {
     .join('');
   refs.gallery.insertAdjacentHTML('beforeend', list);
 };
-
 refs.loadMore.addEventListener('click', onClickLoadMore);
